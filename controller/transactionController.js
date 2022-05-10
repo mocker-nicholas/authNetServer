65466ed4dc1c6ac1107cdb24e5a0274dc905c63e
Nick Orth <nicholas.r.orth@gmail.com>
2022-05-09 20:25:59 -0500

    add more test data

--- controller/transactionController.js
+++ controller/transactionController.js
@@ -7,7 +7,16 @@ import {
   searchUnsettledTransactions,
   formatTransactions,
 } from "../util.js";
-import { zip, card } from "../data.js";
+import {
+  zip,
+  card,
+  first,
+  last,
+  company,
+  street,
+  city,
+  state,
+} from "../data.js";
 
 export const generateTransaction = async () => {
   const amount = ((Math.random() * 10000) / 100).toFixed(2);
@@ -26,22 +35,22 @@ export const generateTransaction = async () => {
           },
         },
         billTo: {
-          firstName: "Test",
-          lastName: "Customer",
-          company: "Google",
-          address: "12 Main Street",
-          city: "Lenexa",
-          state: "KS",
+          firstName: pickRand(first),
+          lastName: pickRand(last),
+          company: pickRand(company),
+          address: pickRand(street),
+          city: pickRand(city),
+          state: pickRand(state),
           zip: pickRand(zip),
           country: "US",
         },
         shipTo: {
-          firstName: "Test",
-          lastName: "Customer",
-          company: "Google",
-          address: "12 Main Street",
-          city: "Lenexa",
-          state: "KS",
+          firstName: pickRand(first),
+          lastName: pickRand(last),
+          company: pickRand(company),
+          address: pickRand(street),
+          city: pickRand(city),
+          state: pickRand(state),
           zip: "66215",
           country: "US",
         },

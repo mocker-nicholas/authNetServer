d3fd2b02d7a60643f2ed182cdddbc09ba2581032
Nick Orth <nicholas.r.orth@gmail.com>
2022-05-31 13:20:07 -0500

    Move vt/hosted page request into mvc config and out of app file

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import transactionRouter from "./routes/transactionrouter.js";
-import { getFormToken } from "./controller/vtcontroller.js";
+import vtRouter from "./routes/vtrouter.js";
 
 const app = express();
 dotenv.config();
@@ -10,15 +10,16 @@ dotenv.config();
 app.use(express.json());
 app.use(cors({ origin: "http://localhost:3000" }));
 app.use("/api/transaction", transactionRouter);
+app.use("/api/vt", vtRouter);
 
 app.get("/", (req, res) => {
   res.send("authNetServer is online");
 });
 
-app.post("/vt/hosted", async (req, res) => {
-  const response = await getFormToken(req.body);
-  return res.json(response);
-});
+// app.post("/vt/hosted", async (req, res) => {
+//   const response = await getFormToken(req.body);
+//   return res.json(response);
+// });
 
 app.listen(8080, () => {
   console.log("on port 8080");
--- routes/vtrouter.js
+++ routes/vtrouter.js
@@ -1,4 +1,7 @@
 import express from "express";
 const router = express.Router();
+import { getHostedToken } from "../controller/vtcontroller.js";
+
+router.route("/hosted").post(getHostedToken);
 
 export default router;

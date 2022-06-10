da38ab5309a9b06f7136d2f1b8327dd97391495b
Nick Orth <nicholas.r.orth@gmail.com>
2022-06-10 14:27:23 -0500

    Add customer detail route, add get customer request

--- controller/customercontroller.js
+++ controller/customercontroller.js
@@ -1,4 +1,8 @@
-import { createCustomerWPayment, getCustomers } from "../util.js";
+import {
+  createCustomerWPayment,
+  getCustomers,
+  getCustomerProfile,
+} from "../util.js";
 
 export const createCustomer = async (req, res, next) => {
   const response = await createCustomerWPayment(req.body);
@@ -7,5 +11,12 @@ export const createCustomer = async (req, res, next) => {
 
 export const getAllCustomers = async (req, res, next) => {
   const response = await getCustomers();
+  return res.json(response);
+};
+
+export const getSingleCustomer = async (req, res, next) => {
+  const { id } = req.params;
+  console.log(id);
+  const response = await getCustomerProfile(id);
   return res.json(response);
 };
--- routes/customerRouter.js
+++ routes/customerRouter.js
@@ -2,11 +2,13 @@ import express from "express";
 import {
   createCustomer,
   getAllCustomers,
+  getSingleCustomer,
 } from "../controller/customercontroller.js";
 
 const router = express.Router();
 
 router.route("/").get(getAllCustomers);
 router.route("/create").post(createCustomer);
+router.route("/:id").get(getSingleCustomer);
 
 export default router;

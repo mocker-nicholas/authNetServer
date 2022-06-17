7548c9d1d91aefc7ae69c9fd7ea3476cf3d6c817
Nick Orth <nicholas.r.orth@gmail.com>
2022-06-17 17:24:01 -0500

    change database name

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ export const connection = mysql.createConnection({
   host: process.env.HOST,
   user: process.env.USER,
   password: process.env.DB_SECRET,
-  database: process.env.DB,
+  database: "defaultdb",
   port: 25060,
 });
 
